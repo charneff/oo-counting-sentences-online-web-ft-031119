b97cb360ffe30a827d5547ce1e538077c928849e
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-08 23:57:16 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,9 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    self.split(/\.|\?|\!/)
+    binding.pry
+    # .delete_if {|w| w.size < 2}.size
   end
 end
 
